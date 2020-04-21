3b34551c03ca7b609658cda4886611d13645e28a
zouqs <zouquansheng@126.com>
2020-04-21 21:56:19 +0800

    Eureka服务发现

--- cloud-provider-payment8001/src/main/java/com/zou/springcloud/controller/PaymentControlller.java
+++ cloud-provider-payment8001/src/main/java/com/zou/springcloud/controller/PaymentControlller.java
@@ -3,14 +3,18 @@ package com.zou.springcloud.controller;
 import com.zou.springcloud.bean.Payment;
 import com.zou.springcloud.bean.CommentResult;
 import com.zou.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-
+import java.util.List;
+@Slf4j
 @RestController
 public class PaymentControlller {
 
@@ -19,6 +23,9 @@ public class PaymentControlller {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommentResult create(Payment payment){
@@ -38,6 +45,19 @@ public class PaymentControlller {
         }else{
             return new CommentResult(444,"查询失败112221id="+id+",serverPort="+serverPort,null);
         }
+    }
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+      List<String> servers =  discoveryClient.getServices();
+        for (String server : servers) {
+            log.info("微服务名称："+server);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-provider-payment");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return discoveryClient;
     }
 
 }
